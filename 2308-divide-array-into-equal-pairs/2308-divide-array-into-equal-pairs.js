75229344b3b1062f18237efb732e8e685ebc02f8
Muhammed Aslam <148055467+muhammedaslam-ap@users.noreply.github.com>
2025-03-17 16:02:58 +0530

    Time: 3 ms (82.95%), Space: 56 MB (23.04%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.js
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.js
@@ -3,26 +3,14 @@
  * @return {boolean}
  */
 var divideArray = function(nums) {
-    freq ={}
-    for(i of nums){
-        if(freq[i]){
-            freq[i]++
-        }else{
-            freq[i]=1
+    const freq = new Map();
+    for (const num of nums) {
+        freq.set(num, (freq.get(num) || 0) + 1);
+    }
+    for (const count of freq.values()) {
+        if (count % 2 !== 0) {
+            return false;
         }
     }
-    let value = Object.values(freq)
-    let isNOtEven=0
-    for( curr of value){
-        if(curr%2==1){
-            isNOtEven=1
-        }
-    }
-
-
-    if(isNOtEven==0){
-        return true
-    }else{
-        return false
-    }
+    return true;
 };
